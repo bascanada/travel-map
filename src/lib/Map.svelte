d96703c3ecdd26dcb467716d04d4e0623bcba479
William Quintal <william95quintalwilliam@outlook.com>
2025-07-19 18:38:27 -0400

    map in component

---
+++ src/lib/Map.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import maplibregl from 'maplibre-gl';
+  let mapContainer: HTMLDivElement;
+
+  onMount(() => {
+    const map = new maplibregl.Map({
+      container: mapContainer,
+      style: {
+        version: 8,
+        sources: {
+          osm: {
+            type: 'raster',
+            tiles: [
+              'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            ],
+            tileSize: 256,
+            attribution: '© OpenStreetMap contributors'
+          }
+        },
+        layers: [
+          {
+            id: 'osm',
+            type: 'raster',
+            source: 'osm'
+          }
+        ]
+      },
+      center: [-0.1276, 51.5074], // London
+      zoom: 5
+    });
+  });
+</script>
+
+<style>
+  #map {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
+
+<div id="map" bind:this={mapContainer}></div>
